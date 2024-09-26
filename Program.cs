ec4df2afafa907d4b3e3618a09f5d945e6435dfb
KRISH BIKRAM SAH <118278451+sahkrish68@users.noreply.github.com>
2024-09-26 15:23:56 +0545

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using BloodManagementSystem.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BloodManagementSystem
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmSplash());
+        }
+    }
+}
